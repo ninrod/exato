c9f28623e7e09c7a85ecc7aa6108f71ec728dcc0
ninrod <filipe.silva@gmail.com>
2017-11-15 20:01:49 -0200

    add commentary

--- evil-textobj-xmlattr.el
+++ evil-textobj-xmlattr.el
@@ -26,6 +26,13 @@
 
 ;;; Commentary:
 
+;; this is a scratch implementaion for an evil text object for hmtl/xml tag attributes,
+;; inspired by https://github.com/whatyouhide/vim-textobj-xmlattr
+
+;; examples:
+;; dax: delete around xml tag attribute
+;; dix: delete inside xml tag attribute
+
 ;;; Code:
 
 ;; }}}
